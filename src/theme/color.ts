deb32723ad35189f7a52dcee5004a3a8a35167fa
SUPRØ <00youareworngtho00@gmail.com>
2026-05-06 10:05:05 +0530

    feat(theme): add color palette

---
+++ src/theme/color.ts
@@ -0,0 +1,9 @@
+export const Color = {
+	primary: "#DC143C",
+	primaryDark: "#760B20",
+	secondary: "#0B1F3A",
+	background: "#151821",
+	tertiary: "#484848",
+	tertiaryDark: "#2E2E2E",
+	primaryWhite: "#D9D9D9",
+};
