9a3d9ced86810c88b3bb43a0186501776018928f
SUPRØ <00youareworngtho00@gmail.com>
2026-05-07 00:20:04 +0530

    feat(components): add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,96 @@
+import { 
+	type PressableProps, 
+	Pressable, 
+	StyleSheet 
+} from 'react-native';
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    withTiming,
+    Easing,
+} from 'react-native-reanimated';
+import LinearGradient, { 
+	type LinearGradientProps 
+} from 'react-native-linear-gradient';
+import { type ReactNode } from 'react';
+import { Color } from '@/theme/color';
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
+type Props = PressableProps & LinearGradientProps & {
+    style?: PressableProps['style'];
+	gradientStyle?: LinearGradientProps['style'];
+    children?: ReactNode;
+    scaleOnPress?: number;
+    duration?: number;
+}
+
+const Button = ({
+    children,
+    style,
+	gradientStyle,
+    onPress,
+    colors,
+    start,
+    end,
+    locations,
+    scaleOnPress = 0.98,
+    duration = 150,
+    ...rest
+}: Props) => {
+    const scale = useSharedValue(1);
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [{ scale: withTiming(scale.value, {
+            duration,
+            easing: Easing.out(Easing.quad),
+        }) }],
+    }));
+
+    return (
+        <AnimatedPressable
+            style={[
+                styles.button,
+                animatedStyle,
+                typeof style === 'function' ? 
+					style({ pressed: false }) : 
+					style,
+            ]}
+            onPress={onPress}
+            onPressIn={() => { scale.value = scaleOnPress; }}
+            onPressOut={() => { scale.value = 1; }}
+            {...rest}
+        >
+            <LinearGradient
+                colors={colors ?? [Color.primary, Color.primaryDark]}
+                start={start}
+                end={end}
+                locations={locations}
+				style={[
+					styles.gradientFill,
+					gradientStyle
+				]}
+            >
+                {children}
+            </LinearGradient>
+        </AnimatedPressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+		width: 100,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 25,
+        overflow: 'hidden',
+    },
+    gradientFill: {
+        ...StyleSheet.absoluteFill,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default Button;
