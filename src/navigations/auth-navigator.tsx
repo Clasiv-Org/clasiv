947654b5e6d9081fe41c4e1fa9ddb970664b6538
SUPRØ <00youareworngtho00@gmail.com>
2026-05-22 20:43:56 +0530

    feat(navigations): add new screens for AuthNavigator

--- src/navigations/auth-navigator.tsx
+++ src/navigations/auth-navigator.tsx
@@ -1,21 +1,47 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingScreen from '@/screens/landing';
-import Login from '@/screens/login';
-import Signup from '@/screens/activate';
-import type { AuthStackParamList } from '@/types/navigation';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LandingScreen from "@/screens/landing";
+import LoginScreen from "@/screens/login";
+import ActivateScreen from "@/screens/activate";
+import OnboardingScreen from "@/screens/onboarding";
+import type { AuthStackParamList } from "@/types/navigation";
+import { Color } from "@/theme/color";
+import EmailVerifyScreen from "@/screens/email-verify";
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthNavigator = () => {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator 
+			initialRouteName="Landing" 
+			screenOptions={{ 
+				headerShown: false,
+				contentStyle: { 
+					backgroundColor: Color.background 
+				},
+				animation: "none",
+			}}
+		>
 			<Stack.Screen name="Landing" component={LandingScreen} />
             <Stack.Screen 
 				name="Login"  
-				component={Login} 
+				component={LoginScreen} 
 				options={{ keyboardHandlingEnabled: true }} 
 			/>
-            <Stack.Screen name="Activate" component={Signup} />
+            <Stack.Screen 
+				name="Activate" 
+				component={ActivateScreen} 
+                options={{ keyboardHandlingEnabled: true }}
+			/>
+			<Stack.Screen 
+				name="Onboarding" 
+				component={OnboardingScreen} 
+				options={{ keyboardHandlingEnabled: true }}
+			/>
+			<Stack.Screen 
+				name="EmailVerify" 
+				component={EmailVerifyScreen} 
+				options={{ keyboardHandlingEnabled: true }}
+			/>
         </Stack.Navigator>
     );
 };
