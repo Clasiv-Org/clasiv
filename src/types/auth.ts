d002799c2710334a6e97c1bf51aabf48196b629a
SUPRØ <00youareworngtho00@gmail.com>
2026-05-23 11:02:09 +0530

    feat(types): update auth types

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,6 +1,36 @@
 import { z } from "zod";
-import { UserProfileSchema } from "@/types/users";
+import { UserProfileSchema, UserSchema } from "@/types/users";
 import { HTTPResponseSchema } from "@/types/http";
+
+const TokenSchema = z.object({
+    accessToken: z.string(),
+    refreshToken: z.string(),
+});
+
+export const ActivationFormSchema = z.object({
+    userName: z.string(),
+    emailId: z.string().email(),
+    phoneNo: z.string(),
+});
+
+export const ActivationInitiateSchema = z.object({
+    userName: z.string(),
+    password: z.string().min(8, 'Password is required'),
+});
+
+export const ActivationEmailRequestOtpSchema = z.object({
+    activationSessionId: z.string(),
+    emailId: z.string().email(),
+});
+
+export const ActivationEmailVerifyOtpSchema = z.object({
+    activationSessionId: z.string(),
+	otp: z.string(),
+});
+
+export const ActivationCompleteSchema = ActivationFormSchema.extend({
+    activationSessionId: z.string(),
+});
 
 export const LoginSchema = z.object({
     identifier: z.string(),
@@ -16,13 +46,22 @@ export const LoginSchema = z.object({
     };
 });
 
+export const ActivateInitiateResponseSchema = HTTPResponseSchema.extend({
+    activationSessionId: z.string(),
+    user: UserSchema,
+});
+
 export const LoginResponseSchema = HTTPResponseSchema.extend({
-	tokens: z.object({
-        accessToken: z.string(),
-        refreshToken: z.string(),
-	}),
+	tokens: TokenSchema,
 	user: UserProfileSchema,
 });
 
+export type ActivationForm = z.infer<typeof ActivationFormSchema>;
+export type ActivationInitiatePayload = z.infer<typeof ActivationInitiateSchema>;
+export type ActivationEmailRequestOtpPayload = z.infer<typeof ActivationEmailRequestOtpSchema>;
+export type ActivationEmailVerifyOtpPayload = z.infer<typeof ActivationEmailVerifyOtpSchema>;
+export type ActivationCompletePayload = z.infer<typeof ActivationCompleteSchema>;
 export type LoginPayload = z.infer<typeof LoginSchema>;
+
+export type ActivationInitiateResponse = z.infer<typeof ActivateInitiateResponseSchema>;
 export type LoginResponse = z.infer<typeof LoginResponseSchema>;
