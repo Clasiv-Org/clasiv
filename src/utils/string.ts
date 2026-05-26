d86edfdf66a3b59fe2225ce7af1805d08f49a59f
SUPRØ <00youareworngtho00@gmail.com>
2026-05-26 15:32:06 +0530

    feat(utils): add string utils, capitalizeFirstLetter

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,6 +1,13 @@
-export function capitalizeWords(str: string): string {
-	return str
-		.split(" ")
-		.map(word => word.charAt(0).toUpperCase() + word.slice(1))
-		.join(" ")
-}
+const capitalizeWords = (str: string): string => {
+    return str
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+};
+
+const capitalizeFirstLetter = (str: string): string => {
+    if (!str) return "";
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export { capitalizeWords, capitalizeFirstLetter };
