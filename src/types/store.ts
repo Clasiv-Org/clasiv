5bdc3d6549a70573bed74e2057a397c2c886f55e
SUPRØ <00youareworngtho00@gmail.com>
2026-05-27 01:55:46 +0530

    feat(types): add type for Toast Store

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,13 +1,18 @@
-import { 
+import type { 
 	ActivationInitiatePayload, 
 	LoginPayload, 
     ActivationForm
 } from "@/types/auth";
-import { 
+import type { 
     ActivationUser,
 	User, 
 	UserProfile 
 } from "@/types/users";
+import type { 
+	ToastType, 
+	ToastProps, 
+    ToastPosition
+} from "@/types/toast";
 
 export type AuthStore = {
 	user: UserProfile | null;
@@ -29,4 +34,16 @@ export type AuthStore = {
 	activationComplete: (password: string) => Promise<void>;
 	login: (data: LoginPayload) => Promise<void>;
 	clear: () => void;
+};
+
+export type ToastStore = {
+	visible: boolean;
+	title: string | null;
+	message: string | null;
+    type: ToastType;
+    duration: number;
+	position: ToastPosition;
+
+    show: (props: ToastProps) => void;
+    hide: () => void;
 };
